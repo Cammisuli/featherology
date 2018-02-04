6dc17542c8934895814dc04b689bdbd9526bc57a
Jonathan Cammisuli <jon@cammisuli.ca>
2018-02-04 01:20:27 -0500

    chore(deps): update dependencies, include new ones

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
+    siteMetadata: {
+        title: `Gatsby Default Starter`
     },
-    {
-      resolve: "gatsby-plugin-typescript",
-      options: {
-        transpileOnly: false,
-        compilerOptions: {
-          target: 'es5'
-        }
-      }
-    },
-    'gatsby-plugin-styled-components'
-  ]
-}
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography.js`
+            }
+        },
+        'gatsby-plugin-typescript',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `img`,
+                path: `${__dirname}/src/images/`
+            }
+        },
+        'gatsby-plugin-styled-components',
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`
+    ]
+};
--- src/declartions.d.ts
+++ src/declartions.d.ts
@@ -1,4 +1,5 @@
 declare module 'react-photo-gallery';
+declare module 'bricks.js';
 declare const graphql: (query: TemplateStringsArray) => void;
 
 declare module '*.jpg' {
@@ -6,13 +7,32 @@ declare module '*.jpg' {
     export = img;
 }
 
-declare module JSX {
-    interface IntrinsicElements {
-      "index": any
-      "badge": any
-      "banner": any;
-      "rentals": any;
-      "about": any;
-      "rental": any;  
+declare module 'gatsby-image' {
+    export interface GatsbyImage {
+        resolutions?: object;
+        sizes?: object;
+        /**Defaults to fading in the image on load**/
+        fadeIn?: boolean;
+        title?: string /**Passed to the img element */;
+        alt?: string /**Passed to the img element */;
+        className?: string | object; //Passed to the wrapper div. Object is needed to support Glamor's css prop
+        outerWrapperClassName?: string | object; //Passed to the outer wrapper div. Object is needed to support Glamor's css prop
+        style?: object; //Spread into the default styles in the wrapper div
+        position?: string; //Defaults to relative. Pass in absolute to make the component absolute positioned
+        backgroundColor?: string | boolean; //	Set a colored background placeholder. If true, uses "lightgray" for the color. You can also pass in any valid color string.
+        onLoad?: Function; //	func	A callback that is called when the full-size image has loaded.
     }
-  }
+    const Img: React.StatelessComponent<GatsbyImage>;
+    export default Img;
+}
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        index: any;
+        badge: any;
+        banner: any;
+        rentals: any;
+        about: any;
+        rental: any;
+    }
+}
